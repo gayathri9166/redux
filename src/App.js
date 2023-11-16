f39e33cca222d3aa6f245a265968015cca398586
Gayathri Gujjari <gayathri.gujjari@otsi.com>
2023-11-16 15:32:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+import {connect} from 'react-redux';
+import { IncAction } from './actions';
+import { DecAction } from './actions';
+const App = ({local_variable,IncAction,DecAction}) => {
+  return(
+    <div>
+      <center>
+        <h1>{local_variable}</h1> <br/>
+        <button onClick={()=>IncAction(5)} >Increment</button>
+        <button onClick={DecAction} >Decrement</button>
+        </center>
     </div>
   );
-}
-
-export default App;
+};
+const mapStateToProps = (state) => ({
+  local_variable : state,
+});
+export default connect(mapStateToProps,{IncAction,DecAction})(App);
